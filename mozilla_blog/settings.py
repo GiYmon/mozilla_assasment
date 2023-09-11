d8ec9b1d2fb4871b007740231559bcbc4c451162
Szymon <szymongilo@gmail.com>
2023-09-11 21:51:58 +0200

    added postgres db

--- mozilla_blog/settings.py
+++ mozilla_blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "mozilla_blog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "example",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
