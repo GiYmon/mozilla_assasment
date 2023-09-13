1903f0e710d1e48119577ea74de63307e9646dfb
Szymon <szymongilo@gmail.com>
2023-09-13 13:38:11 +0200

    added HomePageView

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import RequestFactory, SimpleTestCase, TestCase
+from django.urls import resolve, reverse
 
 from .models import Post
+from .views import HomePageView
 
 User = get_user_model()
 
@@ -44,3 +46,25 @@ class PostModelTest(TestCase):
         self.assertEqual(second_saved_item.title, "test2")
         self.assertEqual(second_saved_item.description, "This is the second post")
         self.assertEqual(second_saved_item.author, self.user)
+
+
+class BlogHomePageTest(SimpleTestCase):
+    def setUp(self) -> None:
+        url = reverse("home")
+        self.response = self.client.get(url)
+        self.factory = RequestFactory()
+
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "home.html")
+        self.assertContains(self.response, "home page")
+
+    def test_homepage_does_not_contains_incorrect_html(self):
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
+
+    def test_homepage_url_resolves_homepage(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
